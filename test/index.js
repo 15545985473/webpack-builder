b36412e83ff36b1d3d349842831504052b2376a5
shuailangpeng <langpeng.shuai@shopee.com>
2021-04-14 23:16:49 +0800

    temp

--- test/index.js
+++ test/index.js
@@ -3,7 +3,9 @@ const path = require('path');
 describe('Array', function() {
   const config = require(path.resolve(__dirname, '../webpack.config'));
   it('path', function() {
+
     assert.equal(config.entry.index, '/Users/langpengshuai/Desktop/webpack-builder/src/index/index.js');
     assert.equal(config.entry.search, '/Users/langpengshuai/Desktop/webpack-builder/src/search/index.js')
+    
   });
 });
