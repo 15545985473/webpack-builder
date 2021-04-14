7500d0d0e0885bd852556c7a3d2ac2b889648ce1
shuailangpeng <langpeng.shuai@shopee.com>
2021-04-14 22:56:42 +0800

    path change

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 describe('Array', function() {
   const config = require(path.resolve(__dirname, '../webpack.config'));
   it('path', function() {
-    assert.equal(config.entry.index, '/Users/langpengshuai/Desktop/webpack-study/src/index/index.js');
-    assert.equal(config.entry.search, '/Users/langpengshuai/Desktop/webpack-study/src/search/index.js')
+    assert.equal(config.entry.index, '/Users/langpengshuai/Desktop/webpack-builder/src/index/index.js');
+    assert.equal(config.entry.search, '/Users/langpengshuai/Desktop/webpack-builder/src/search/index.js')
   });
 });
